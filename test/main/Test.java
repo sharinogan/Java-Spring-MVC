3b219eb343f68b6c14f066c7c301aba826093468
Voragun Supawong <voragun2543@gmail.com>
2022-08-30 21:18:38 +0700

    add more test

--- test/main/Test.java
+++ test/main/Test.java
@@ -7,4 +7,28 @@ public class Test {
         double result = c.m4("70");
         assert result == 70;
     }
+    
+    public void test2() {
+        CoffeeController c = new CoffeeController();
+        double result = c.m4("100");
+        assert result == 95.0;
+    }
+    
+    public void test3() {
+        CoffeeController c = new CoffeeController();
+        double result = c.m4("180");
+        assert result == 171.0;
+    }
+    
+    public void test4() {
+        CoffeeController c = new CoffeeController();
+        double result = c.m4("101");
+        assert result == 95.95;
+    }
+    
+    public void test5() {
+        CoffeeController c = new CoffeeController();
+        double result = c.m4("Love");
+        assert result == -1.0;
+    }
 }
