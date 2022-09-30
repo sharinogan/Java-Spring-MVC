c6aa07e75cd486f8009485f2dfdd0ad619b0eda3
Voragun <voragun2543@gmail.com>
2022-09-30 17:43:50 +0700

    try to use freemarker

--- code/main/Center.java
+++ code/main/Center.java
@@ -1,14 +1,43 @@
 package main;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Controller
-class Center {
+class Web {
+    @RequestMapping({"/", "/home"})
+    String showIndex() {
+        String[] list = {"/", "/home"};
+        return "display";
+    }
     
+    @RequestMapping("/staff")
+    String showStaff(Model m) {
+        m.addAttribute("company", "iCoffee");
+        m.addAttribute("location", "Bangkok");
+        String[] data = { "Bill G", "Mark Z", "James B" };
+        m.addAttribute("staff", data);
+        return "display-staff";
+    }
 }
 
 @RestController
 class WebService {
     
+}
+
+class Product {
+    double price;
+    double getSpecialPrice() {
+        return price * 0.90;
+    }
+}
+
+class Book extends Product {
+    @Override
+    double getSpecialPrice() {
+        return price * 0.85;
+    }
 }
