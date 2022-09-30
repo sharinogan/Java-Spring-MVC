c73537f6b027c5f3ea5d7f2f5ba67165aeb8fa12
Voragun <voragun2543@gmail.com>
2022-09-30 16:23:32 +0700

    [NEW] setup and use webflux

---
+++ code/main/Center.java
@@ -0,0 +1,14 @@
+package main;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+class Center {
+    
+}
+
+@RestController
+class WebService {
+    
+}
--- code/main/Start.java
+++ code/main/Start.java
@@ -7,9 +7,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 class Start {
-    public static void main(String[] data) {
-        ApplicationContext context;     // environment
-        context = SpringApplication.run(Setup.class);
+    public static void main(String[] cookies) {
+        var context = SpringApplication.run(Setup.class);
     }
 }
 
@@ -20,34 +19,8 @@ class Setup {
 
 @RestController
 class Sample {
-    
-    @RequestMapping("/check")
-    int  m1() {
-        Runtime r = Runtime.getRuntime();
-        return r.availableProcessors();
-    }
-    
-    // https://localhost:30000/coffee/order?price=350
-    @RequestMapping("/coffee/order")
-    String m2(String price) {
-        try {
-            double p = Double.valueOf(price);
-            p = p * 0.90;
-            return "Total is " + p;
-        } catch (Exception e) {
-            return "Invalid Number";
-        }
-    }
-    
-    // https://localhost:30000/coffee/total?price=350&shipping=40
-    @RequestMapping("/coffee/total")
-    String m3(String price, String shipping) {
-        try {
-            double p = Double.valueOf(price);
-            double s = Double.valueOf(shipping);
-            return "Total for this product is " + (p + s);
-        } catch (Exception e) {
-            return "You not enter the price or shipping value!!!";
-        }
+    @RequestMapping("/sample")
+    int showSample() {
+        return 4567;
     }
 }
